d3f0896682769fc12f80720df80480c25f4e340c
Kevin Qi <kevinqi@utexas.edu>
2020-10-24 15:55:23 -0500

    basic skeleton added

---
+++ iqualify/src/pages/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class home extends Component {
+    render() {
+        return (
+            <h1 className="jibran">
+                Landing Page
+            </h1>
+        )
+    }
+}
